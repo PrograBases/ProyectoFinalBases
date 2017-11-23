fc38795b8097f240ea50266b12c7e41f7921da16
nlrarojas <nlrarojas@hotmail.com>
2017-11-22 22:44:12 -0600

    Implementando el ingreso de flujos

---
+++ ProyectoFinalBases/Proyecto Final Bases de Datos/view/IngresoFlujos.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Proyecto_Final_Bases_de_Datos.view
+{
+    partial class IngresoFlujos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dB_PRODDataSet1 = new Proyecto_Final_Bases_de_Datos.DB_PRODDataSet1();
+            this.fLUJOMASTERBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.fLUJO_MASTERTableAdapter = new Proyecto_Final_Bases_de_Datos.DB_PRODDataSet1TableAdapters.FLUJO_MASTERTableAdapter();
+            this.idFlujoMaestroDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.codigoFlujoMasterTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nuevoFlujoMasterTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_PRODDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fLUJOMASTERBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingreso de flujos de procesos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Código del flujo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(378, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Flujos maestros";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idFlujoMaestroDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.fLUJOMASTERBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(381, 97);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(145, 267);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dB_PRODDataSet1
+            // 
+            this.dB_PRODDataSet1.DataSetName = "DB_PRODDataSet1";
+            this.dB_PRODDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // fLUJOMASTERBindingSource
+            // 
+            this.fLUJOMASTERBindingSource.DataMember = "FLUJO_MASTER";
+            this.fLUJOMASTERBindingSource.DataSource = this.dB_PRODDataSet1;
+            // 
+            // fLUJO_MASTERTableAdapter
+            // 
+            this.fLUJO_MASTERTableAdapter.ClearBeforeFill = true;
+            // 
+            // idFlujoMaestroDataGridViewTextBoxColumn
+            // 
+            this.idFlujoMaestroDataGridViewTextBoxColumn.DataPropertyName = "IdFlujoMaestro";
+            this.idFlujoMaestroDataGridViewTextBoxColumn.HeaderText = "IdFlujoMaestro";
+            this.idFlujoMaestroDataGridViewTextBoxColumn.Name = "idFlujoMaestroDataGridViewTextBoxColumn";
+            this.idFlujoMaestroDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // codigoFlujoMasterTxt
+            // 
+            this.codigoFlujoMasterTxt.Location = new System.Drawing.Point(54, 203);
+            this.codigoFlujoMasterTxt.Name = "codigoFlujoMasterTxt";
+            this.codigoFlujoMasterTxt.Size = new System.Drawing.Size(100, 20);
+            this.codigoFlujoMasterTxt.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Código flujo master";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(564, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Nuevo flujo maestro";
+            // 
+            // nuevoFlujoMasterTxt
+            // 
+            this.nuevoFlujoMasterTxt.Location = new System.Drawing.Point(567, 107);
+            this.nuevoFlujoMasterTxt.Name = "nuevoFlujoMasterTxt";
+            this.nuevoFlujoMasterTxt.Size = new System.Drawing.Size(100, 20);
+            this.nuevoFlujoMasterTxt.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // IngresoFlujos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 519);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nuevoFlujoMasterTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.codigoFlujoMasterTxt);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "IngresoFlujos";
+            this.Text = "IngresoFlujos";
+            this.Load += new System.EventHandler(this.IngresoFlujos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_PRODDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fLUJOMASTERBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DB_PRODDataSet1 dB_PRODDataSet1;
+        private System.Windows.Forms.BindingSource fLUJOMASTERBindingSource;
+        private DB_PRODDataSet1TableAdapters.FLUJO_MASTERTableAdapter fLUJO_MASTERTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idFlujoMaestroDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox codigoFlujoMasterTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nuevoFlujoMasterTxt;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ProyectoFinalBases/Proyecto Final Bases de Datos/view/IngresoFlujos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Final_Bases_de_Datos.view
+{
+    public partial class IngresoFlujos : Form
+    {
+        DBConnection dbConnection = new DBConnection();
+
+        public IngresoFlujos()
+        {
+            InitializeComponent();
+        }
+
+        private void IngresoFlujos_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dB_PRODDataSet1.FLUJO_MASTER' table. You can move, or remove it, as needed.
+            this.fLUJO_MASTERTableAdapter.Fill(this.dB_PRODDataSet1.FLUJO_MASTER);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            codigoFlujoMasterTxt.Text = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nuevoFlujoMaster = Int32.Parse(nuevoFlujoMasterTxt.Text);
+            dbConnection.insertFlujoMaster(Int32.Parse(nuevoFlujoMasterTxt.Text));
+            dbConnection.desconectar();
+            this.actualizarGrid();
+        }
+
+        private void actualizarGrid()
+        {
+            dbConnection.actualizarDataGridView(this.dataGridView1, "Select * From FLUJO_MASTER");
+        }
+    }
+}
